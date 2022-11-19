50bba717d10b77a6efda993c108c9945d856e027
Rob Jackson <rob@rjackson.me>
2022-11-19 14:03:29 +0000

    Turns out one regex was enough afterall

--- src/Parser.php
+++ src/Parser.php
@@ -11,22 +11,13 @@ use Rjackson\CsvNameParser\Data\Person;
 class Parser
 {
   /**
-   * Patterns against which to match names, from most to least specific
-   *
-   * We could try to write a mega-regex to do this, but mulitple simpler patterns
-   * will be easier to maintain.
-   *
-   * @var string[]
+   * Regex pattern to identify name segments, where:
+   * - title is mandatory
+   * - firstName is optional, and either a full word or initialied
+   * - middleName is optional, and either a full word or initialied
+   * - lastName is mandatory
    */
-  protected array $patterns = [
-    // Single name where
-    // - title is mandatory
-    // - firstName is optional, and either a full word or initialied
-    // - middleName is optional, and either a full word or initialied
-    // - lastName is mandatory
-    "/^(?<title>\w+)\.?\s+?((?<firstName>\w+)\.?\s+)?((?<middleName>\w+)\.?\s+)?(?<lastName>[\w\-–—]+)$/",
-  ];
-
+  protected string $namePattern = "/^(?<title>\w+)\.?\s+?((?<firstName>\w+)\.?\s+)?((?<middleName>\w+)\.?\s+)?(?<lastName>[\w\-–—]+)$/";
   /**
    * Parse free-text input and try to extract a Person record from it.
    *
@@ -59,25 +50,21 @@ class Parser
    */
   function parseSingle(string $input): ?Person
   {
-    foreach ($this->patterns as $pattern) {
-      preg_match($pattern, $input, $matches);
-      if (empty($matches)) {
-        continue;
-      }
-
-      // middleName is deliberately not extracted because we don't need it
-      ["title" => $title, "firstName" => $firstName, "lastName" => $lastName] = $matches;
-
-      // Derive initial from first name
-      $initial = $firstName[0] ?? null;
-      if (mb_strlen($firstName) <= 1) {
-        $firstName = null;
-      }
-
-      return new Person($title, $firstName, $initial, $lastName);
+    preg_match($this->namePattern, $input, $matches);
+    if (empty($matches)) {
+      return null;
     }
 
-    return null;
+    // middleName is deliberately not extracted because we don't need it
+    ["title" => $title, "firstName" => $firstName, "lastName" => $lastName] = $matches;
+
+    // Derive initial from first name
+    $initial = $firstName[0] ?? null;
+    if (mb_strlen($firstName) <= 1) {
+      $firstName = null;
+    }
+
+    return new Person($title, $firstName, $initial, $lastName);
   }
 
   /**
